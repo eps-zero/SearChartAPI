b805227c65d0d720b91429782f5776b868b307f6
Muhammed <muhammed@ahmed.ahmed>
2023-09-13 12:53:29 +0400

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # SearChartAPI
 # SearChartAPI
 # SearChartAPI
+# SearChartAPI
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from dashboard.models import Sect, SubSect, Indica, Country
 
-# Register your models here.
+@admin.register(Sect)
+class CountryRatingAdmin(admin.ModelAdmin):
+    list_display = ('sector',)
+    search_fields = ('sector',)
+
+@admin.register(SubSect)
+class CountryRatingAdmin(admin.ModelAdmin):
+    list_display = ('sector', 'subsector')
+    list_filter = ('sector',)
+    search_fields = ('sector', 'subsector')
+
+@admin.register(Country)
+class CountryRatingAdmin(admin.ModelAdmin):
+    list_display = ('country', 'country_code', 'year', 'indicator', 'amount','rank')
+    list_filter = ('country', 'year', 'indicator', 'rank')
+    search_fields = ('country', 'year', 'indicator')
+
+
+@admin.register(Indica)
+class CountryRatingAdmin(admin.ModelAdmin):
+    list_display = ('sector', 'subsector', 'indicator')
+    list_filter = ('sector', 'subsector')
+    search_fields = ('sector', 'subsector', 'indicator')
+
